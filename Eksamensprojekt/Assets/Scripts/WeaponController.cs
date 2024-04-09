a50d7d0528d82eb90e12baf14ba57f11a94d9a05
JonathanRune <75438628+JonathanRune@users.noreply.github.com>
2024-04-09 13:07:48 +0200

    Snowdrift

--- Eksamensprojekt/Assets/Scripts/WeaponController.cs
+++ Eksamensprojekt/Assets/Scripts/WeaponController.cs
@@ -13,7 +13,6 @@ public class WeaponController : MonoBehaviour
     Animator anim;
 
     [SerializeField] Transform cameraHolder;
-    [SerializeField] float hitDistance = 5;
     [SerializeField] LayerMask enemyLayerMask;
     [SerializeField] Vector3 boxSize;
 
